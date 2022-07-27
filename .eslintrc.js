569d31bf82dc323e0db4f3756bd8a723d440a6e6
Code pianist <77950104+1739616529@users.noreply.github.com>
2022-07-27 11:15:12 +0800

    风格统一

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,42 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": "latest"
+        ecmaVersion: "latest",
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "arrow-spacing": ["error", { "before": true, "after": true }],
+    plugins: ["react", "@typescript-eslint"],
+    rules: {
+        "arrow-spacing": ["error", { before: true, after: true }],
         "@typescript-eslint/no-non-null-assertion": "off",
-        "space-in-parens": [2, "always"],
+        "space-in-parens": [2, "never"],
         "space-before-blocks": [2, "always"],
         "object-curly-spacing": [2, "always"],
         "no-trailing-spaces": 1,
         "no-multi-spaces": 1,
         "no-mixed-spaces-and-tabs": [2, false],
-        "key-spacing": [2, { "beforeColon": false, "afterColon": true }],
+        "key-spacing": [2, { beforeColon: false, afterColon: true }],
         "no-unused-vars": "off",
         "brace-style": [1, "1tbs"],
         "no-nested-ternary": 2,
-        "eqeqeq": [2, "allow-null"],
-        "comma-spacing": [2, { "before": false, "after": true }],
+        eqeqeq: [2, "allow-null"],
+        "comma-spacing": [2, { before: false, after: true }],
         "array-bracket-spacing": [2, "never"],
-        "react/react-in-jsx-scope": "off",
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+        "react/react-in-jsx-scope": "on",
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+    },
 };
