0a0ad7bf6c4d00b7fe4a3a1b939175ee688230ed
Code pianist <77950104+1739616529@users.noreply.github.com>
2022-07-27 13:55:22 +0800

    更新 react 前端 页面

--- src/jsx.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            item: React.DetailedHTMLProps<HTMLElement>;
-        }
-    }
-}
